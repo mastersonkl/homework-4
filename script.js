ed32af8ab93716aa32c7db19f344111fef95df6f
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-23 20:00:42 -0400

    Updated html doc for the quiz

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
+$(document).ready(function() {
+    var seconds = 120;
+    var interval = 0;
 
-window.onload = function () {
-    $("#start-button").on("click", game.triviaTimer);
-    $("#start-button").click(game.triviaTimer);
-  
-};
+    
+    function startTimer() {
+        if (seconds === 120) {
+          interval = setInterval(function () {
+            seconds--;
+    
+            renderTime();
+          }, 1000);
+        }
+      }
 
-var game = {
+     
 
-    timer: 0,
-    seconds: 10,
-    seconds: 45,
 
-    triviaTimer: function () {
-        $("#start-button").hide();
-        $("#timer").html('<div>00:' + game.seconds + '</div>').addClass('theTime');
-        $("#timer").css('visibility', 'visible');
-        $("#timer").html('<div>00:' + game.seconds);
-        game.timer = setInterval(game.count, 1000);
-        
-    },
-
-   
-    count: function () {
-        game.seconds--;
-        game.checkTime();
-        $("#timer").html('<div>00:' + game.seconds + '</div>').addClass('theTime');
-        $("#timer").html('<div>00:' + game.seconds);
-        if (game.seconds < 1) {
-            game.stopGame();
-        };
-    },
-
-    checkTime: function () {
-        if (game.seconds < 10) {
-            game.seconds = "0" + game.seconds
-        };
-        return game.seconds;
-    },
-}
-
+})
