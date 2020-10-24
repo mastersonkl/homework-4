1bd7a88aa7486549c7bd069d4c74902b4f2516cf
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-23 20:15:39 -0400

    start timer function

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-    var seconds = 120;
-    var interval = 0;
-
+  var timer = $(".timer");
+  var startButton = $(".startBtn");
+  var seconds = 120;
+var interval;
     
-    function startTimer() {
-        if (seconds === 120) {
-          interval = setInterval(function () {
-            seconds--;
-    
-            renderTime();
-          }, 1000);
-        }
-      }
+function startTimer() {
+  if (seconds === 120) {
+    interval = setInterval(function () {
+      seconds--;
 
+      renderTime();
+    }, 1000);
+  }
+
+  console.log(seconds);
+}
+
+function renderTime() {
+  timer.text("Timer:" + seconds);
+}
+
+startButton.on("click", startTimer);
      
 
 
